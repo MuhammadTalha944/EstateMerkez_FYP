cf21b2426b62a14c5a2224eea669fa05cd43c713
MuhammadTalha944 <talhaishtiaq944@gmail.com>
2018-05-14 08:59:16 +0500

    no message

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'driver',
     'pyAarz',
     'ZameenScraper',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -149,3 +149,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
